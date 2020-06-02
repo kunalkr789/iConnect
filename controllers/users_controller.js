3f1fa8f90e11d1f234c39ee9c0666ffa944f287b
Kunal Kumar <kunalkr789@gmail.com>
2020-06-02 20:21:14 +0530

    create forms for sign in and sign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,19 @@ module.exports.profile = function(req, res){
         title: 'user profile'
     });
 }
+
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: 'Sign Up'
+    });
+}
+
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: 'Sign In'
+    });
+}
+//get the sign up data
+module.exports.create = function(req, res){
+    
+}
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const usersController = require('../controllers/users_controller');
 
 router.get('/profile', usersController.profile);
-
+router.get('/sign-in', usersController.signIn);
+router.get('/sign-up', usersController.signUp);
 
 
 module.exports = router;
