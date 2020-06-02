fe3e82e9f4f013110d716abd0f9f0ae103405e99
Kunal Kumar <kunalkr789@gmail.com>
2020-06-02 18:09:28 +0530

    add controller for users

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
-
+const userController = require('../controllers/users_controller');
 
 router.get('/', homeController.home);
 
-
+router.use('/users', require('./users'));
 
 module.exports = router;
